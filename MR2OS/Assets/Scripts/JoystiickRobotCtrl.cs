6b0192d607dfcd880b4a91466bacf95ebd691023
KousukeImori <imorikousuke1655@gmail.com>
2025-04-15 19:53:16 +0900

    iPad用UIの作成(Joystickの値に応じてパブリッシュ)
    Joystickの値に応じて、信号をパブリッシュするようにした

---
+++ MR2OS/Assets/Scripts/JoystiickRobotCtrl.cs
@@ -0,0 +1,173 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Robotics.ROSTCPConnector;
+using RosMessageTypes.Std;
+using Oculus.Interaction.PoseDetection;
+
+public class JoystiickRobotCtrl : MonoBehaviour
+{
+    ROSConnection ros;
+    public string topicName = "crawler_ctrl";
+    public FixedJoystick inputMove; //JoyStickの値を取得
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        ros = ROSConnection.GetOrCreateInstance();
+        ros.RegisterPublisher<Int8Msg>(topicName);
+        Time.fixedDeltaTime = 0.1f;
+    }
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //前進,後退（高速、低速）
+        if(inputMove.Horizontal < 0.2)
+            if(inputMove.Horizontal > -0.2)
+                if(inputMove.Vertical >= 0)//前進
+                {
+                    if (inputMove.Vertical > 0.2)
+                    {
+                        RightMotorForward_high();
+                        LeftMotorForward_high();
+                    }else{
+                        RightMotorForward_low();
+                        LeftMotorForward_low();
+                    }
+
+                }else if(inputMove.Vertical < 0)//後退
+                {
+                    if (inputMove.Vertical < -0.2)
+                    {
+                        RightMotorBack_high();
+                        LeftMotorBack_high();
+                    }else{
+                        RightMotorBack_low();
+                        LeftMotorBack_low();
+                    }
+                }
+        // 右旋回
+        if (inputMove.Horizontal >= 0.2)
+            if(inputMove.Vertical >=0)
+            {
+                LeftMotorForward_high(); //
+                RightMotorForward_low();
+            }else{
+                LeftMotorBack_high();
+                RightMotorBack_low();
+            }
+        // 左旋回
+        if (inputMove.Horizontal <= -0.2 )
+            if (inputMove.Vertical >= 0)
+            {
+                RightMotorForward_high();
+                LeftMotorForward_low();
+            }else{
+                RightMotorBack_high();
+                LeftMotorBack_low();
+            }
+  
+// 超新地旋回用　あとでコメントアウトするかも  
+        if (inputMove.Vertical < 0.2)
+            if (inputMove.Vertical > -0.2)
+            {
+                if (inputMove.Horizontal >= 0)
+                {
+                    LeftMotorForward_low();
+                    RightMotorBack_low();
+                }else if (inputMove.Horizontal < 0){
+                    RightMotorForward_low();
+                    LeftMotorBack_low();
+                }
+            }
+            
+        else{
+            RightMotorStop();
+            LeftMotorStop();
+        }
+    }
+
+    public void RightMotorStop()
+    {
+        Int8Msg int8Message = new Int8Msg(0);
+        ros.Publish(topicName, int8Message);
+        Debug.Log($"Published: {0}");
+    }
+
+    public void RightMotorForward_low()
+    {
+        Int8Msg int8Message = new Int8Msg(1);
+        ros.Publish(topicName, int8Message);
+    }
+
+    public void RightMotorForward_high()
+    {
+        Int8Msg int8Message = new Int8Msg(2);
+        ros.Publish(topicName, int8Message);
+    }
+
+    public void RightMotorBack_low()
+    {
+        Int8Msg int8Message = new Int8Msg(3);
+        ros.Publish(topicName, int8Message);
+    }
+
+    public void RightMotorBack_high()
+    {
+        Int8Msg int8Message = new Int8Msg(4);
+        ros.Publish(topicName, int8Message);
+    }
+
+    public void LeftMotorStop()
+    {
+       Int8Msg int8Message = new Int8Msg(10);
+       ros.Publish(topicName, int8Message);
+       Debug.Log($"Published: {1}");
+    }
+
+    public void LeftMotorForward_low()
+    {
+        Int8Msg int8Message = new Int8Msg(11);
+        ros.Publish(topicName, int8Message);
+    }
+
+    public void LeftMotorForward_high()
+    {
+        Int8Msg int8Message = new Int8Msg(12);
+        ros.Publish(topicName, int8Message);
+    }
+
+    public void LeftMotorBack_low()
+    {
+        Int8Msg int8Message = new Int8Msg(13);
+        ros.Publish(topicName, int8Message);
+    }
+
+    public void LeftMotorBack_high()
+    {
+        Int8Msg int8Message = new Int8Msg(14);
+        ros.Publish(topicName, int8Message);
+    }
+
+    public void ShutdownGpioChip()
+    {
+        Int8Msg int8Message = new Int8Msg(20);
+        ros.Publish(topicName, int8Message);
+    }
+
+    private void OnApplicationQuit()
+    {
+        RightMotorStop();
+        ShutdownGpioChip();
+        Debug.Log("stop Crawler-Ctrl-System");
+    }
+/*
+.Vertical,.HorizonalがJoyStickの垂直方向、水平方向の入力に対応しており、
+JoyStickの傾き具合でそれぞれ-1~1のFloat型で返す。
+*/
+}
+
+
+
